88062166ef7990c781498937ba0676543fd4a21a
Nathaniel-V-Williams <158172837+Nathaniel-V-Williams@users.noreply.github.com>
2024-01-31 23:28:43 +0200

    index.html
    new changes

---
+++ script.py
@@ -0,0 +1,23 @@
+<script>
+    // Get the button
+    var goToTopBtn = document.getElementById("goToTopBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            goToTopBtn.style.display = "block";
+        } else {
+            goToTopBtn.style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    goToTopBtn.onclick = function() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+    };
+</script>
